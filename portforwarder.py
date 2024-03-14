ed2d4aded14fd2575466f2f78f77403dc0922d4d
vitaly <optinsoft@gmail.com>
2024-03-14 22:51:42 +0500

    v.1.1 --allow-ip, --allow-any-ip

--- portforwarder.py
+++ portforwarder.py
@@ -3,11 +3,17 @@ import argparse
 from pathlib import Path
 import threading
 
-__version__ = "1.0"
+__version__ = "1.1"
 __module__ = Path(__file__).stem
 
-async def handle_client(reader, writer, target_host, target_port):
+async def handle_client(reader, writer, target_host, target_port, allowed_ip_list, allow_any_ip):
     try:
+        client_ip = writer.get_extra_info('peername')[0]
+        print(f"Client connected on IP {client_ip}")
+        if not allow_any_ip:
+            if not client_ip in allowed_ip_list:
+                print(f"Client IP denied: {client_ip}")
+                return            
         target_reader, target_writer = await asyncio.open_connection(target_host, target_port)
         await asyncio.gather(
             relay(reader, target_writer),
@@ -34,9 +40,9 @@ def console_input():
         if len(cmd) > 0:
             if "q" == cmd: break
 
-async def start_server(source_host, source_port, target_host, target_port):
+async def start_server(source_host, source_port, target_host, target_port, allowed_ip_list, allow_any_ip):
     server = await asyncio.start_server(
-        lambda reader, writer: handle_client(reader, writer, target_host, target_port),
+        lambda reader, writer: handle_client(reader, writer, target_host, target_port, allowed_ip_list, allow_any_ip),
         source_host, source_port
     )
 
@@ -59,6 +65,9 @@ if __name__ == "__main__":
     parser.add_argument('--source-port', type=int, required=True, help='Source port to listen on')
     parser.add_argument('--target-host', type=str, required=True, help='Target port to forward connections to')
     parser.add_argument('--target-port', type=int, required=True, help='Target port to forward connections to')
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('--allow-ip', nargs="+", type=str, help='List of allowed IP addresses')
+    group.add_argument('--allow-any-ip', action='store_true', help='Allow access from any IP address')
 
     args = parser.parse_args()
 
@@ -66,6 +75,7 @@ if __name__ == "__main__":
         args.source_host, 
         args.source_port, 
         args.target_host, 
-        args.target_port)
+        args.target_port,
+        args.allow_ip,
+        args.allow_any_ip)
     )
-    
